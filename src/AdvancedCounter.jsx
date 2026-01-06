e0f7d4a56b08be6b6ae473ca969e5728b8bca4d3
Tyler Stallworth <ps.tstallworth@outlook.com>
2026-01-06 11:27:54 -0500

    Functional

--- src/AdvancedCounter.jsx
+++ src/AdvancedCounter.jsx
@@ -4,17 +4,29 @@ export default function AdvancedCounter() {
   const [count, setCount] = useState(0);
   const [history, setHistory] = useState([0]);
   const [saveStatus, setSaveStatus] = useState("");
+  const [step,setStep] = useState(1);
 
   const handleIncrement = () => {
-    setCount(prev => prev + 1);
+    setCount(prev => prev + step);
   };
 
   const handleDecrement = () => {
-    setCount(prev => prev - 1);
+    setCount(prev => prev - step);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+    setHistory([0]);
+    setSaveStatus("");
   };
 
   useEffect(() => {
-    setHistory(prevHistory => [ ...prevHistory, count]);
+    setHistory(prev => {
+      if (prev[prev.length - 1] !== count) {
+        return [... prev.slice(-49), count];
+      } 
+      return prev;
+    });
   }, [count]); 
 
   useEffect(() => {
@@ -47,22 +59,69 @@ export default function AdvancedCounter() {
     }, []);
 
     return (
-      <div>
+      <div
+        style={{
+          border: "2px solid #ffe600ff",
+          borderRadius: "12px",
+          padding: "20px",
+          maxWidth: "400px",
+          margin: "20px auto",
+          backgroundColor: "#911cb4ff",
+          boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+          fontFamily: "sans-serif",
+          display: "flex",
+          flexDirection: "column",
+          gap: "12px"
+        }}
+      >
         <h1>Advanced Counter</h1>
 
         <p>Current Count: {count}</p>
 
+        <label>Step Amount: </label>
+        <input
+          type="number"
+          value={String(step)}
+          onChange={(e) => {
+            const raw = e.target.value;
+            const cleaned = Number(raw);
+            setStep(cleaned);
+          }}
+        />
+        <div style={{ marginTop: "10px", display: "flex", gap: "8px", flexWrap: "wrap" }}>
         <button onClick={handleIncrement}>Increment</button>
         <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={handleReset}
+        style={{
+          background: "#e74c3c",
+          color: "white",
+          border: "none",
+          padding: "2px 6px",
+          fontSize: "0.9rem",
+          borderRadius: "6px",
+          cursor: "pointer",
+        }}
+        >
+          Reset
+        </button>
 
         <p>{saveStatus}</p>
 
         <h2>Count History:</h2>
-        <p>{history.join(",")}</p>
+        <p
+        style={{
+          wordWrap: "break-word",
+          overflowWrap: "break-word",
+          whiteSpace: "normal",
+          maxSpace: "normal",
+          maxWidth: "100%"
+        }}
+       > 
+         {history.join(",")}
+       </p>
 
         <p> Use ArrowUp to increment and ArrowDown to decrement.</p>
       </div>
+    </div>
     );
-  }  
-
-
+  }
